e9af9ee2d3c6508117d3bd0fe1b987afb32998a5
SofianeMohamedStd <namoune.sofianemohmaed@gmail.com>
2020-12-11 11:11:36 +0100

    create WalletTest

--- parisSportifCode/src/Entity/Wallet.php
+++ parisSportifCode/src/Entity/Wallet.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\WalletRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=WalletRepository::class)
@@ -15,10 +16,27 @@ class Wallet
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private $id;
+    private ?int $id;
+    /**
+     * @ORM\Column(type="integer", length=10)
+     * @Assert\PositiveOrZero
+     */
+    private ?int $credit;
+
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getCredit () : ?int
+    {
+        return $this -> credit;
+    }
+
+    public function setCredit ( ?int $credit ) : self
+    {
+        $this -> credit = $credit*100;
+        return $this;
     }
 }
--- parisSportifCode/tests/Entity/WalletTest.php
+++ parisSportifCode/tests/Entity/WalletTest.php
@@ -4,7 +4,67 @@
 namespace App\Tests\Entity;
 
 
-class WalletTest
+use App\Entity\Wallet;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class WalletTest extends KernelTestCase
 {
+    private $validator;
+
+    protected function setUp(): void
+    {
+
+        $kernel = self::bootKernel();
+        $kernel->boot();
+        $this->validator = $kernel->getContainer()->get("validator");
+    }
+
+    public function testInstanceOf()
+    {
+        $wallet = new Wallet();
+        $this->assertInstanceOf(Wallet::class, $wallet);
+        $this->assertClassHasAttribute("credit", Wallet::class);
+    }
+
+    /**
+     * @dataProvider providerInvalidCredit
+     * @param $credit
+     */
+    public function testInvalidCredit ($credit)
+    {
+        $wallet = new Wallet();
+        $wallet->setCredit($credit);
+        $errors = $this->validator->validate($wallet);
+        $this->assertGreaterThanOrEqual(1, count($errors));
+    }
+
+    public function providerInvalidCredit()
+    {
+        return [
+            [-254]
+        ];
+    }
+
+    /**
+     * @dataProvider providerValidCredit
+     * @param $credit
+     */
+    public function testValidCredit ($credit)
+    {
+        $wallet = new Wallet();
+        $wallet->setCredit($credit);
+        $errors = $this->validator->validate($wallet);
+        $this->assertEquals(0, count($errors));
+    }
+
+    public function providerValidCredit()
+    {
+        return [
+            [0],
+            [10.99],
+            [254]
+        ];
+    }
+
 
 }
