a661783246062db74a384fd736b05584a3159e5e
mkayz68 <m.kaizerli@gmail.com>
2020-12-15 20:29:44 +0100

    vérif de l'entité pas encore fini

---
+++ parisSportifCode/tests/Entity/SportCollectifTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\SportCollectif;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class SportCollectifTest extends KernelTestCase
+{
+    private $validator;
+
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+        $kernel->boot();
+        $this->validator = $kernel->getContainer()->get("validator");
+    }
+
+    public function testInstanceOf()
+    {
+        $sport = new SportCollectif();
+        $this->assertInstanceOf(SportCollectif::class, $sport);
+
+    }
+
+    /**
+     * @param $nombreJoueurs
+     */
+    public function testNombresJoueursIsNotNull()
+    {
+       $sport = new SportCollectif();
+       $errors = $this->validator->validate($sport, null, "nbJoueurs");
+       $this->assertEquals(0, count($errors));
+    }
+
+    public function testNombresJoueursIsInvalid()
+    {
+        $sport = new SportCollectif();
+        $errors = $this->validator->validate($sport, null, "nbJoueurs");
+        $this->assertEquals(0, count($errors));
+    }
+}
