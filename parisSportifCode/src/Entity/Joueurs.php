3b6463d815b314994738605424dcc14f877d0d4c
mkayz68 <m.kaizerli@gmail.com>
2020-12-16 16:08:35 +0100

    entité Joueurs

---
+++ parisSportifCode/src/Entity/Joueurs.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JoueursRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=JoueursRepository::class)
+ */
+class Joueurs
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\Regex(
+     *     pattern="/^[a-zA-ZÀ-ÿ-]{2,16}$/")
+     * @Groups({"naming"})
+     */
+    private ?string $name;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\Regex(
+     *     pattern="/^[a-zA-ZÀ-ÿ-]{2,16}$/")
+     * @Groups({"naming"})
+     */
+    private ?string $prenom;
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+    public function setPrenom($prenom): self
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+
+}
---
+++ parisSportifCode/tests/Entity/EquipeTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Tests\Entity;
+
+
+use App\Entity\Equipe;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class EquipeTest extends KernelTestCase
+{
+    private $validator;
+
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+        $kernel->boot();
+        $this->validator = $kernel->getContainer()->get("validator");
+    }
+
+    /**
+     * @test
+     */
+    public function equipeInstanceOf()
+    {
+        $equipe = new Equipe();
+        $this->assertInstanceOf(Equipe::class, $equipe);
+    }
+
+    /**
+     * @param $name
+     * @dataProvider provideNameValidEquipeValues
+     */
+    public function testNameValidEquipe($name)
+    {
+        $equipe = new Equipe();
+        $equipe->setName($name);
+        $errors = $this->validator->validate($equipe, null, "naming");
+        $this->assertEquals(0, count($errors));
+    }
+
+    public function provideNameValidEquipeValues(): array
+    {
+        return [
+            ['om'],
+            ['PSG'],
+        ];
+    }
+
+
+}
