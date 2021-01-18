2f2ade2cb5e24a4c1fbfbef7adb17578d5aec349
SofianeMohamedStd <namoune.sofianemohmaed@gmail.com>
2021-01-18 05:38:32 +0100

    entity + controller wallet

---
+++ parisSportifCode/migrations/Version20210118012319.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210118012319 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE wallet ADD id_user_id INT NOT NULL, DROP draw_bet, DROP with_add_earnings');
+        $this->addSql('ALTER TABLE wallet ADD CONSTRAINT FK_7C68921F79F37AE5 FOREIGN KEY (id_user_id) REFERENCES user (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_7C68921F79F37AE5 ON wallet (id_user_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE wallet DROP FOREIGN KEY FK_7C68921F79F37AE5');
+        $this->addSql('DROP INDEX UNIQ_7C68921F79F37AE5 ON wallet');
+        $this->addSql('ALTER TABLE wallet ADD draw_bet INT DEFAULT NULL, ADD with_add_earnings INT DEFAULT NULL, DROP id_user_id');
+    }
+}
