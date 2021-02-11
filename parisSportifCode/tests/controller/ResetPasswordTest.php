47a6f809a67ee375421ae0f0d361551c7c3c2b75
sisso <namoune.sofianemohamed@gmail.com>
2021-02-11 10:21:45 +0100

    2021-02-11 #1 correction de la relation copetition Sport Evenement

--- parisSportifCode/tests/controller/ResetPasswordTest.php
+++ parisSportifCode/tests/controller/ResetPasswordTest.php
@@ -4,7 +4,32 @@
 namespace App\Tests\controller;
 
 
-class ResetPasswordTest
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ResetPasswordTest extends webTestCase
 {
+    public function testRestPasswordPage()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/reset-password');
+        $this->assertResponseStatusCodeSame(200);
+
+    }
+
+    public function testSendMailForRestPassword()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET','/reset-password');
+
+        $form = $crawler->filter('form')->form();
+
+        $form['reset_password_request_form[email]'] = 'sofiane1@gmail.com';
+
+        $client->submit($form);
+        $this->assertEmailCount(1);
+
+        $this->assertResponseRedirects('/reset-password/check-email');
+
+    }
 
 }
